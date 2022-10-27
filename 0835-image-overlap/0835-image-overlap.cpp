3cc5de0081e9870acdf009bbf918780d5a021835
mohitkota143 <96908137+mohitkota143@users.noreply.github.com>
2022-10-27 13:31:36 +0530

    Time: 261 ms (35.42%), Space: 12.5 MB (22.22%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,3 +1,19 @@
+/*
+Linear Transformation:
+find cells with ones. to move a 1 from A to a 1 from B, it'll take some transformation  (dx, dy).  
+for another 1 cell in A to also overlap with a 1 cell in B will take the SAME (dx,dy). 
+make a map of possible (dx,dy) and count the ones that have the same (dx,dy)
+
+find [[i,j]...] of 1s for A, B
+find all combos of each filtered array
+calc the dx,dy and set to a map.  find the mapping that has the highest count and return
+
+T: filtering takes O(N^2). combos takes a double for loop of N^2 * N^2 in the worst case. 
+so N^2 + N^2 * N^2 ---> O(N^4)
+S: O(N^2) for arrays space in the worst case of A and B filled with 1s
+*/
+
+
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
