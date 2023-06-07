621b40efe146c88fbb45744138330c7362910359
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-06-07 23:02:45 +0530

    Time: 730 ms (10.80%), Space: 167.9 MB (29.85%) - LeetHub

---
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) 
+    {
+        int m = mat.size();
+        int n = mat[0].size();
+        int arr_size = arr.size();
+        
+        map<int,pair<int,int>>mp;
+        map<int,int>row , col ;
+        // each value points to its index in 2d array
+        for(int i=0;i<m;i++)
+        {   row[i] = 0;
+            for(int j=0;j<n;j++)
+            {
+                col[j] = 0; 
+                mp[mat[i][j]] = {i,j};
+            }
+        }
+        
+        for(int i=0;i<arr_size;i++)
+        {
+            auto indexs = mp[arr[i]];
+            
+            // paint in mat
+            mat[indexs.first][indexs.second] = -1;
+            row[indexs.first] += 1;
+            col[indexs.second] += 1;
+            if(row[indexs.first]==n  or col[indexs.second]==m)
+            {
+                return i;
+            }
+        }
+        
+        return 0;
+        
+        
+        
+    }
+};
