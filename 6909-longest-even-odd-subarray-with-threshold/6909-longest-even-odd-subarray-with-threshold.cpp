cb7ef711e969adf9e67d6d99ccf9080770894d72
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-07-02 10:35:59 +0530

    Time: 116 ms (50.00%), Space: 91 MB (100.00%) - LeetHub

--- 6909-longest-even-odd-subarray-with-threshold/6909-longest-even-odd-subarray-with-threshold.cpp
+++ 6909-longest-even-odd-subarray-with-threshold/6909-longest-even-odd-subarray-with-threshold.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    bool isvalid(int l,int r,vector<int>&nums,int threshold)
-    {
-        bool con1 = true;
-        bool con2 = true;
-        if(nums[l]%2!=0)
-        {
-            return false;
-        }
-        for(int start=l;start<r;start++)
-        {
-            if(nums[start]%2==nums[start+1]%2)
-            {
-                con1=false;
-                break;
-            }
-            if(nums[start]>threshold)
-            {
-                con2 = false;
-                break;
-            }
-        }
-        if(nums[r]>threshold)
-        {
-            con2 = false;
-        }
-        return con1&&con2;
-    }
+    
     int longestAlternatingSubarray(vector<int>& nums, int threshold) 
     {
         int n = nums.size();
         int max_len = 0;
-        for(int l=0;l<n;l++)
+        int last = INT_MAX;
+        if(nums[0] <= threshold && nums[0] % 2 == 0) last = 0;
+        int j = 1;
+        while(j<n)
         {
-            for(int r=l;r<n;r++)
+            if(nums[j]>threshold || ((nums[j] % 2) == (nums[j-1] % 2)))
             {
-                if(isvalid(l,r,nums,threshold))
-                {
-                    max_len = max(max_len,r-l+1);    
-                }
+                max_len = max(max_len,j-last);
+                last = INT_MAX;
             }
+            if(last==INT_MAX && nums[j]<=threshold && nums[j]%2==0)
+            {
+               last = j;
+            }
+            j = j+1;
         }
-        
+        max_len = max(max_len,j-last);
         return max_len;
     }
 };
