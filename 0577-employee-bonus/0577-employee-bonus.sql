7fa675020709f354c8ff2311252fd1331c252e44
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-08-21 14:52:28 +0530

    Time: 2413 ms (26.54%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select e1.name , e2.bonus from Employee e1 left outer join Bonus e2 on e1.empId = e2.empId where e2.bonus<1000 or e2.bonus is NULL group by e1.empId;  
+select e1.name , e2.bonus from Employee e1 left outer join Bonus e2 on e1.empId = e2.empId where e2.bonus<1000 or e2.bonus is NULL;  
