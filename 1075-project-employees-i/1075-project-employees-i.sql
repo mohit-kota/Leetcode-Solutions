df2ca1b5045c642d2c4d53630169ffae60686094
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-08-23 11:05:35 +0530

    Time: 1652 ms (56.58%), Space: 0B (100.00%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select e1.project_id , ROUND(SUM(e2.experience_years)/COUNT(e2.experience_years),2) as average_years from Project e1 join Employee e2 on e2.employee_id = e1.employee_id group by e1.project_id;
