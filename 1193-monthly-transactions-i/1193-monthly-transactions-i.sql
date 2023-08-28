9fe303d6199e3b569eef0d0871573e4904aeec61
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-08-28 17:59:59 +0530

    Time: 2431 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,1 @@
+select SUBSTR(trans_date,1,7) as month , country , count(id) as trans_count , SUM(CASE WHEN state='approved' then 1 else 0 END) as approved_count , SUM(amount) as trans_total_amount , SUM(CASE WHEN state='approved' then amount else 0 END) as approved_total_amount from Transactions group by month,country;
