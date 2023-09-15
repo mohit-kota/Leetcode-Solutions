bde4d71e7341121bbffa1abc3e1b55177fefd9f7
MOHIT KOTA <96908137+mohit-kota@users.noreply.github.com>
2023-09-15 12:54:11 +0530

    Time: 680 ms (88.79%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-select s1.id-1 as id ,s1.student from Seat s1 where s1.id mod 2 = 0
-union 
-select s2.id+1 as id ,s2.student from Seat s2 where s2.id mod 2 = 1 and s2.id != (select MAX(id) from Seat)
-union 
-select s3.id as id   ,s3.student from Seat s3 where s3.id mod 2 = 1 and s3.id = (select max(id) from Seat) 
-order by id asc;  
+# select s1.id-1 as id ,s1.student from Seat s1 where s1.id mod 2 = 0
+# union 
+# select s2.id+1 as id ,s2.student from Seat s2 where s2.id mod 2 = 1 and s2.id != (select MAX(id) from Seat)
+# union 
+# select s3.id as id   ,s3.student from Seat s3 where s3.id mod 2 = 1 and s3.id = (select max(id) from Seat) 
+# order by id asc;  
+
+
+
+
+select row_number() over(order by if(mod(id,2)=0,id-1,id+1) ) as id, student from Seat;
